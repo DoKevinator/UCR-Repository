729ce4a2a5d7833d80159839491dfba88832cb02
Kevin Do <kevin34554@yahoo.com>
2013-05-13 16:19:11 -0600

    Update main.cpp

--- CS14/Assignment-4-Tree-Calculator/main.cpp
+++ CS14/Assignment-4-Tree-Calculator/main.cpp
@@ -1,15 +1,15 @@
 // Course: CS 14 Spring 2013
 //
-// First name: <<INSERT>>
-// Last name: <<INSERT>>
-// Course username: <<INSERT>>
-// Email address: <<INSERT>>
+// First name: Kevin
+// Last name: Do
+// Course username: kdo009
+// Email address: kdo009@ucr.edu
 //
-// Lecture section: <<INSERT>>
-// Lab section: <<INSERT>>
-// TA: <<INSERT>>
+// Lecture section: 001
+// Lab section: 021
+// TA: Steve Cook
 //
-// Assignment: <<INSERT>>
+// Assignment: assn4
 //
 // I hereby certify that the code in this file
 // is ENTIRELY my own original work.
