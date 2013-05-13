6b75c9c9f7e4582245eb66107179db26dd554825
Kevin Do <kevin34554@yahoo.com>
2013-05-13 16:17:27 -0600

    Update syntaxtree.h

--- CS14/Assignment-4-Tree-Calculator/syntaxtree.h
+++ CS14/Assignment-4-Tree-Calculator/syntaxtree.h
@@ -1,3 +1,20 @@
+// Course: CS 14 Spring 2013
+//
+// First name: <<INSERT>>
+// Last name: <<INSERT>>
+// Course username: <<INSERT>>
+// Email address: <<INSERT>>
+//
+// Lecture section: <<INSERT>>
+// Lab section: <<INSERT>>
+// TA: <<INSERT>>
+//
+// Assignment: <<INSERT>>
+//
+// I hereby certify that the code in this file
+// is ENTIRELY my own original work.
+//=================================================================
+
 //syntaxtree.h
 
 #ifndef __SYNTAXTEE_H_
